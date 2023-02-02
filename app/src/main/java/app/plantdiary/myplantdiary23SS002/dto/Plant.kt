51de62b39aa188f2f7de5461ffc8d44297b81996
Brandan <brandan.jones@uc.edu>
2023-02-02 10:03:07 -0500

    Create plant data class

---
+++ app/src/main/java/app/plantdiary/myplantdiary23SS002/dto/Plant.kt
@@ -0,0 +1,8 @@
+package app.plantdiary.myplantdiary23SS002.dto
+
+data class Plant(var genus : String, var species : String, var common : String = "") {
+    override fun toString(): String {
+        return common
+    }
+}
+
